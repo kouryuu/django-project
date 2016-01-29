a379e778bf5f44a3474a993e1373aeccc5e7cd59
kouryuu <antarticfreeze@gmail.com>
2016-01-29 12:20:38 -0600

    Login and logout

---
+++ login/static/js/main_panel.js
@@ -0,0 +1,22 @@
+var mainPanelApp = angular.module('mainPanelApp', []);
+mainPanelApp.controller('mainPanelCtrl', ['$scope','$http','$log', function($scope,$http,$log) {
+$scope.show_derivative = false
+$scope.show_item_selection = false
+$scope.show_edit_names = false
+
+$scope.display_edit_names = (function () {
+  $scope.show_item_selection = false
+  $scope.show_derivative = false
+  $scope.show_edit_names = true
+});
+$scope.display_derivative = (function () {
+  $scope.show_derivative = true
+  $scope.show_item_selection = false
+  $scope.show_edit_names = false
+});
+$scope.display_item_selection = (function () {
+  $scope.show_item_selection = true
+  $scope.show_derivative = false
+  $scope.show_edit_names = false
+});
+}]);
